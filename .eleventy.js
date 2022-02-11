a11ff0854270600ec5d0942701f4a5a51873671f
Praveen Puglia <praveen.puglia@sprinklr.com>
2022-02-11 14:38:03 +0530

    cache busting & css prefixing

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
+const pluginCSS = require("eleventy-postcss-extension");
 const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./static": ".",
   });
+
+  // Plugins
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginCSS);
+  eleventyConfig.addPlugin(cacheBuster({}));
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
